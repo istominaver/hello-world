2535f84e64ae0df3871c2fed8d6a1821eaef5c55
nika <istominaVer@gmail.com>
2016-04-04 22:37:47 +0300

    Try add text processing

--- app.js
+++ app.js
@@ -20,9 +20,25 @@ var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'my_bot_1', appSecret: 'b213900f816542e0abf96d6a3d3a9d3b' });
-bot.add('/', function (session) {
-   session.send('Hello World'); 
-});
+bot.add('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                     " you've been programming for " + session.userData.coding + 
+                     " years and use " + session.userData.language + ".");
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
