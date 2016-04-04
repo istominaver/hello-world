1a93b711bb00c420f5e9db8e42b1d2c87d44f7ea
nika <istominaVer@gmail.com>
2016-04-04 18:47:44 +0300

    Add SOCKET.IO

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-/*var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', msg);
- });
-});
-http.listen(port, function(){
- console.log('listening on ' + port);
-});*/
-
 var restify = require('restify');
 var builder = require('botbuilder');
 
